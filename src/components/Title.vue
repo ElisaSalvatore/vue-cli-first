c564e7476480bb5f9555dc868ba9d47d061263d5
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-26 15:24:16 +0100

    created Vue Cli project and printed new title

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">
+    La mia prima app con Vue CLI!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.title {
+  font-size: 35px;
+  font-weight: 600;
+}
+</style>
