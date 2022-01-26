3279ee4d4a82782d6fae9ea811daa15109259bca
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-26 16:37:48 +0100

    added new component 'Hero' (jumbotron)

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jumbotron">
+        Questo è un jumbotron.
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Hero',
+}
+</script>
+
+<style scoped lang="scss">
+    .jumbotron {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50vh;
+        margin-top: 20px;
+        font-size: 25px;
+        background-color: indianred;
+    }
+</style>
